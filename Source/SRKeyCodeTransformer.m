61a7e8b4bf148825b4058f5360f099a3be10f201
Andy Kim <andypotion@gmail.com>
2011-05-06 18:36:07 +0000

    Fix Keyboard shortcuts not recording in non-unicode input methods such as Chinese, Japanese, and Korean. Fixes issue 45. Also fixes a small memory leak.

--- Source/SRKeyCodeTransformer.m
+++ Source/SRKeyCodeTransformer.m
@@ -164,10 +164,20 @@ static NSArray              *padKeysArray        = nil;
 	CFDataRef layoutData;
 	UInt32 keysDown = 0;
 	layoutData = (CFDataRef)TISGetInputSourceProperty(tisSource, kTISPropertyUnicodeKeyLayoutData);
-	if(!layoutData) return nil;
+	
+	CFRelease(tisSource);
+	
+	// For non-unicode layouts such as Chinese, Japanese, and Korean, get the ASCII capable layout
+	if(!layoutData) {
+		tisSource = TISCopyCurrentASCIICapableKeyboardLayoutInputSource();
+		layoutData = (CFDataRef)TISGetInputSourceProperty(tisSource, kTISPropertyUnicodeKeyLayoutData);
+		CFRelease(tisSource);
+	}
 
+	if (!layoutData) return nil;
+	
 	const UCKeyboardLayout *keyLayout = (const UCKeyboardLayout *)CFDataGetBytePtr(layoutData);
-			
+	
 	UniCharCount length = 4, realLength;
 	UniChar chars[4];
 	
